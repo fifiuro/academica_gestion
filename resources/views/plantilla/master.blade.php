1dad04549eac174c0ab9d16045c525ea854c73d3
Christian Cardenas <ccardenas@cognoslp.com.bo>
2018-07-18 08:50:26 -0400

    fecha 18-7-2018

--- resources/views/plantilla/master.blade.php
+++ resources/views/plantilla/master.blade.php
@@ -124,7 +124,7 @@
         </aside>
 
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper" style="min-height:1100px !important">
+        <div class="content-wrapper">
             <!-- Content Header (Page header) -->
             <section class="content-header">
                 <h1>
